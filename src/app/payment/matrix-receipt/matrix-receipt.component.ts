087d0c46e2d313c9cbd76f5c50aa3fb568f07088
Philip <philip.monilla@carlsonwagonlit.com>
2019-03-18 18:59:50 +0800

    feature/apply validation on matrix receipt

--- src/app/payment/matrix-receipt/matrix-receipt.component.ts
+++ src/app/payment/matrix-receipt/matrix-receipt.component.ts
@@ -4,6 +4,8 @@ import { NgbActiveModal, NgbModal,NgbModalRef } from '@ng-bootstrap/ng-bootstrap
 import { UpdateMatrixReceiptComponent } from '../update-matrix-receipt/update-matrix-receipt.component';
 import { RemarkModel } from 'src/app/models/remark.model';
 import { DatePipe } from '@angular/common';
+import { RemarkCollectionService } from 'src/app/service/remark.collection.service';
+import { RemarkGroup } from 'src/app/models/remark.group.model';
 
 @Component({
   selector: 'app-matrix-receipt',
@@ -21,7 +23,7 @@ export class MatrixReceiptComponent implements OnInit, OnChanges {
   private modalRef: NgbModalRef;
   remarkList : Array<RemarkModel>;
 
-  constructor(private modalService: NgbModal) {
+  constructor(private modalService: NgbModal, private remarkCollectionService:RemarkCollectionService) {
     this.matrixReceiptList=new Array<MatrixReceiptModel>();
     this.remarkList= new Array<RemarkModel>();
    }
@@ -30,17 +32,17 @@ export class MatrixReceiptComponent implements OnInit, OnChanges {
   }
 
 buildRemarks(){
-this.remarkList.length=0;
+    this.remarkList.length=0;
 
-this.matrixReceiptList.forEach(matrix => {
-  
-  if (matrix.bankAccount=='224000'){
-      this.processRBCredemptionRemarks(matrix);
-  }else {
-    this.processOtherPaymentRemarks(matrix);
-  }
-
-});
+    this.matrixReceiptList.forEach(matrix => {
+      
+      if (matrix.bankAccount=='224000'){
+          this.processRBCredemptionRemarks(matrix);
+      }else {
+        this.processOtherPaymentRemarks(matrix);
+      }
+     
+    });
 
 }
 
@@ -53,26 +55,31 @@ processRBCredemptionRemarks(matrix:MatrixReceiptModel){
   this.remarkList.push(this.getRemarksModel(rem3));
 }
 
+updateRemarkGroup(){
+  var remGroup = new RemarkGroup();
+  remGroup.group = "Matrix Remark";
+  remGroup.remarks = this.remarkList;
+  this.remarkCollectionService.addUpdateRemarkGroup(remGroup);
+}
 
 processOtherPaymentRemarks(matrix:MatrixReceiptModel){
 
-  enum CardType {
-    VI = '115000',
-    MC = '116000',
-    AMEX = '117000',
-    Diners ='118000'
-}
+        enum CardType {
+          VI = '115000',
+          MC = '116000',
+          AMEX = '117000',
+          Diners ='118000'
+      }
 
-var datePipe = new DatePipe("en-US");
-var fop = ""
-if (Object.values(CardType).includes(matrix.bankAccount))
-{
-  fop = "CC" + matrix.ccNo + '/-EXP' + datePipe.transform(matrix.expDate, 'mmYY');
-}else{
-    fop = 'CA'
-}
+    var datePipe = new DatePipe("en-US");
+    var fop = ""
+    if (Object.values(CardType).includes(matrix.bankAccount))
+    {
+      fop = "CC" + matrix.ccNo + '/-EXP' + datePipe.transform(matrix.expDate, 'mmYY');
+    }else{
+        fop = 'CA'
+    }
 
-alert(fop);
 
   let rem1 = 'REC/-RLN-' + matrix.rln + '/-RF-' + matrix.passengerName+'/-AMT-'+matrix.amount;
   let rem2 = 'REC/-RLN-' + matrix.rln + '/-FOP-'+ fop + '/-LK-T/-BA-'+ matrix.bankAccount + '/-GL-'+matrix.glCode;
@@ -99,13 +106,14 @@ getRemarksModel(remText){
     this.modalRef.componentInstance['title'] ="Add Matrix Receipt";
     matrixReceipt.rln = (this.matrixReceiptList.length +1);
     this.modalRef.componentInstance['matrixReceipt']= matrixReceipt;
-    this.modalRef.result.then(x=>{
-     
+
+    this.modalRef.result.then(x=>{     
       if (typeof  (x) !="string"){
         this.matrixReceiptList.push(x);
+        this.buildRemarks();
+        this.updateRemarkGroup();
       }
-
-
+     
     });
 
   }
--- src/app/payment/update-matrix-receipt/update-matrix-receipt.component.ts
+++ src/app/payment/update-matrix-receipt/update-matrix-receipt.component.ts
@@ -1,9 +1,10 @@
 
 import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { MatrixReceiptModel } from 'src/app/models/matrix-receipt.model';
+import { MatrixReceiptModel, PaymentType } from 'src/app/models/matrix-receipt.model';
 import { SelectItem } from 'src/app/models/select.item.model';
 import { PnrService } from 'src/app/service/pnr.service';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-update-matrix-receipt',
@@ -15,24 +16,90 @@ title:string;
 matrixReceipt: MatrixReceiptModel;
 bankAccountList: Array<SelectItem>;
 passengerList: Array<any>;
-
+matrixForm:FormGroup;
+isSubmitted:boolean;
 
 @ViewChild('bankAccount') bankAccEl: ElementRef;
-  constructor( public activeModal: NgbActiveModal,private pnrService:PnrService) { 
+  constructor( public activeModal: NgbActiveModal,private pnrService:PnrService,private formBuilder: FormBuilder) { 
     this.bankAccountList=new Array<SelectItem>();
     this.matrixReceipt= new MatrixReceiptModel();
     this.loadBankAccount();
+    this.matrixForm = new FormGroup({
+      'bankAccount': new FormControl('',[Validators.required]),
+      'passengerName': new FormControl('', [Validators.required]), 
+      'description': new FormControl('', [Validators.required]), 
+      'cwtRef': new FormControl('', [Validators.required,Validators.minLength(6), Validators.maxLength(6)]), 
+      'points': new FormControl('', [Validators.required,Validators.minLength(7)],), 
+      'lastFourVi': new FormControl('',[Validators.required,Validators.minLength(4), Validators.maxLength(4)]), 
+      'gcNumber': new FormControl('', [Validators.maxLength(19)]), 
+      'amount': new FormControl('', [Validators.required,Validators.min(0),Validators.pattern('[0-9]*')]), 
+      'vendorCode': new FormControl('', [Validators.required,Validators.maxLength(10)]), 
+      'ccNo': new FormControl('', [Validators.required,Validators.minLength(16),Validators.required,Validators.maxLength(16)]), 
+      'expDate': new FormControl('', [Validators.required])
+      
+    },{updateOn:'blur'});
+    
+
+  }
+
+ ngOnInit() {
+     if (!this.pnrService.isPNRLoaded ) this.pnrService.getPNR();
+     this.passengerList = this.pnrService.getPassengers();
+ 
     
   }
 
-  ngOnInit() {
-     if (!this.pnrService.isPNRLoaded ) this.pnrService.getPNR();
-    this.passengerList = this.pnrService.getPassengers();
+get PaymentType() {return PaymentType; }
+
+  bankAccountChange(newValue){
+
+    switch (newValue){
+      case  '224000':
+          this.matrixReceipt.paymentType= PaymentType.Rbc
+          this.enableFormControls(['gcNumber','ccNo','expDate','vendorCode'],true);
+          this.enableFormControls(['cwtRef','points','lastFourVi'],false);
+          break;
+      case '115000':
+      case '116000':
+      case '117000':
+      case '118000':
+           this.matrixReceipt.paymentType = PaymentType.CreditCard
+           this.enableFormControls(['cwtRef','points','lastFourVi'],true);
+           this.enableFormControls(['gcNumber','ccNo','expDate','vendorCode'],false);
+        break;
+      default:
+        this.matrixReceipt.paymentType = PaymentType.Undefined
+        this.enableFormControls(['cwtRef','points','lastFourVi'],true);
+        this.enableFormControls(['ccNo','expDate','vendorCode'],true)
+        this.enableFormControls(['gcNumber'],false);
+        break;
+    }
+
   }
 
+enableFormControls(controls:string[],disabled:boolean){
+  controls.forEach(c => {
+    if (disabled){
+      this.matrixForm.get(c).disable();
+    }else {
+      this.matrixForm.get(c).enable();
+    }
+  });
 
-saveReceipt(){
-  
+}
+
+
+get f() { return this.matrixForm.controls; }
+
+saveReceipt(){  
+  this.isSubmitted=true;
+
+
+  if (this.matrixForm.invalid) {
+    alert("Please Complete And Complete all the required Information");
+    return;
+  }
+
   this.activeModal.close(this.matrixReceipt);
 }
 
@@ -54,5 +121,24 @@ this.bankAccountList = [{itemText:"",itemValue:""},
 
 
 }
+
+
+getAllErrors(form: FormGroup | FormArray): { [key: string]: any; } | null {
+  let hasError = false;
+  const result = Object.keys(this.matrixForm.controls).reduce((acc, key) => {
+      const control = this.matrixForm.get(key);
+      
+      const errors = (control instanceof FormGroup || control instanceof FormArray)
+          ? this.getAllErrors(control)
+          : (control.touched? control.errors :'') ;
+      if (errors) {
+          acc[key] = errors;
+          hasError = true;
+      }
+      return acc;
+  }, {} as { [key: string]: any; });
+  return hasError ? result : null;
+}
+
 
 }

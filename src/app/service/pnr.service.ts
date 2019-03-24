4f5e786799424f7faae9604af10603ac08da9ebc
Stan <stanley.bteh@gmail.com>
2019-03-25 02:57:52 +0800

    US7751
    Modified UI + added check exists in PNRservice

--- src/app/service/pnr.service.ts
+++ src/app/service/pnr.service.ts
@@ -1,179 +1,196 @@
-import { Injectable } from '@angular/core';
-import { R3NgModuleDef } from '@angular/compiler/src/render3/r3_module_compiler';
-import { stringify } from '@angular/core/src/util';
+import { Injectable } from "@angular/core";
+import { R3NgModuleDef } from "@angular/compiler/src/render3/r3_module_compiler";
+import { stringify } from "@angular/core/src/util";
 
 declare var PNR: any;
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: "root"
 })
 export class PnrService {
-    pnrObj: any;
-    isPNRLoaded = false;
-    errorMessage = '';
-    destinationCity = [{ endpoint: "" }]
+  pnrObj: any;
+  isPNRLoaded = false;
+  errorMessage = "";
+  destinationCity = [{ endpoint: "" }];
 
-    constructor() { }
+  constructor() {}
 
-    async getPNR(): Promise<void> {
-        this.pnrObj = new PNR();
-        await this.pnrObj.retrievePNR().then((res: any) => {
-            this.isPNRLoaded = true;
-            this.errorMessage = 'PNR Loaded Successfully';
+  async getPNR(): Promise<void> {
+    this.pnrObj = new PNR();
+    await this.pnrObj.retrievePNR().then(
+      (res: any) => {
+        this.isPNRLoaded = true;
+        this.errorMessage = "PNR Loaded Successfully";
+      },
+      (error: string) => {
+        this.isPNRLoaded = false;
+        this.errorMessage = "Error:" + error;
+      }
+    );
 
-        }, (error: string) => {
-            this.isPNRLoaded = false;
-            this.errorMessage = 'Error:' + error;
-        });
+    console.log(JSON.stringify(this.pnrObj));
+  }
 
-        console.log(JSON.stringify(this.pnrObj));
-    }
-
-    getCFLine() {
-
-        if (this.isPNRLoaded) {
-            for (const rm of this.pnrObj.rmElements) {
-                if (rm.freeFlowText.indexOf('CF/-') === 0) {
-                    return rm.freeFlowText;
-                }
-            }
-
+  getCFLine() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.rmElements) {
+        if (rm.freeFlowText.indexOf("CF/-") === 0) {
+          return rm.freeFlowText;
         }
-        return '';
+      }
     }
+    return "";
+  }
 
-    getSFCLineNumber() {
-        if (this.isPNRLoaded) {
-            for (const rm of this.pnrObj.rmElements) {
-                if (rm.freeFlowText.indexOf('SFC/-') === 0) {
-                    return rm.elementNumber;
-                }
-            }
+  getSFCLineNumber() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.rmElements) {
+        if (rm.freeFlowText.indexOf("SFC/-") === 0) {
+          return rm.elementNumber;
         }
-        return '';
+      }
     }
+    return "";
+  }
 
-    getTaxLineNumber() {
-        if (this.isPNRLoaded) {
-            for (const rm of this.pnrObj.rmElements) {
-                if (rm.freeFlowText.indexOf('TAX') === 0) {
-                    return rm.elementNumber;
-                }
-            }
+  getInsuranceCancellationLineNumber() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.rmElements) {
+        if (rm.freeFlowText.indexOf("U12/-") === 0) {
+          return rm.elementNumber;
         }
-        return '';
+      }
     }
+    return "";
+  }
 
-
-    getFSLineNumber() {
-        if (this.isPNRLoaded) {
-            for (const rm of this.pnrObj.fsElements) {
-                return rm.elementNumber;
-            }
+  getTaxLineNumber() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.rmElements) {
+        if (rm.freeFlowText.indexOf("TAX") === 0) {
+          return rm.elementNumber;
         }
-        return '';
+      }
     }
+    return "";
+  }
 
+  getFSLineNumber() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.fsElements) {
+        return rm.elementNumber;
+      }
+    }
+    return "";
+  }
 
-    getPassengers() {
-        if (this.isPNRLoaded) {
-            const passengers = [];
+  getPassengers() {
+    if (this.isPNRLoaded) {
+      const passengers = [];
 
-            for (let rm of this.pnrObj.nameElements) {
-                let fname = rm.fullNode.enhancedPassengerData.enhancedTravellerInformation.otherPaxNamesDetails.givenName;
-                let lname = rm.fullNode.enhancedPassengerData.enhancedTravellerInformation.otherPaxNamesDetails.surname;
+      for (let rm of this.pnrObj.nameElements) {
+        let fname =
+          rm.fullNode.enhancedPassengerData.enhancedTravellerInformation
+            .otherPaxNamesDetails.givenName;
+        let lname =
+          rm.fullNode.enhancedPassengerData.enhancedTravellerInformation
+            .otherPaxNamesDetails.surname;
 
-                let fullname: any = lname + '-' + fname.toUpperCase().replace(' MS', '').replace(' MRS', '').replace(' MSTR', '').replace(' INF', '').replace(' MR', '').replace(' MISS', '');
+        let fullname: any =
+          lname +
+          "-" +
+          fname
+            .toUpperCase()
+            .replace(" MS", "")
+            .replace(" MRS", "")
+            .replace(" MSTR", "")
+            .replace(" INF", "")
+            .replace(" MR", "")
+            .replace(" MISS", "");
 
-                let passenger = {
-                    firstname: fname,
-                    surname: lname,
-                    id: rm.elementNumber,
-                    fullname: fullname
-                };
-                passengers.push(passenger);
-            }
-            return passengers;
+        let passenger = {
+          firstname: fname,
+          surname: lname,
+          id: rm.elementNumber,
+          fullname: fullname
+        };
+        passengers.push(passenger);
+      }
+      return passengers;
+    }
+    return new Array<string>();
+  }
+
+  getDestinationLine() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.rmElements) {
+        if (rm.freeFlowText.indexOf("DE/-") === 0) {
+          return rm.elementNumber;
         }
-        return new Array<string>();
+      }
     }
+    return "";
+  }
 
+  pushDestination(endpoint) {
+    const look = this.destinationCity.find(x => x.endpoint === endpoint);
+    if (look == null) {
+      var destination = {
+        endpoint: endpoint
+      };
+      this.destinationCity.push(destination);
+    }
+  }
 
-    getDestinationLine() {
-        if (this.isPNRLoaded) {
-            for (const rm of this.pnrObj.rmElements) {
-                if (rm.freeFlowText.indexOf('DE/-') === 0) {
-                    return rm.elementNumber;
-                }
-            }
+  getPnrDestinations() {
+    if (this.isPNRLoaded) {
+      for (let air of this.pnrObj.airSegments) {
+        var airendpoint = air.arrivalAirport;
+        this.pushDestination(airendpoint);
+      }
+
+      for (let rm of this.pnrObj.miscSegments) {
+        // var endpoint = rm.fullNode.itineraryFreetext.boardpointDetail.cityCode;
+        var longFreetext = rm.fullNode.itineraryFreetext.longFreetext;
+        var endpoint = null;
+        if (longFreetext.indexOf("/EC-") > -1) {
+          var endpoint = longFreetext.substr(
+            longFreetext.indexOf("/EC-") + 4,
+            3
+          );
         }
-        return '';
-    }
-
-    pushDestination(endpoint) {
-        const look = this.destinationCity.find(x => x.endpoint === endpoint);
-        if (look == null) {
-            var destination =
-            {
-                endpoint: endpoint
-            }
-            this.destinationCity.push(destination);
+        if (endpoint != null) {
+          this.pushDestination(endpoint);
         }
+      }
+      return this.destinationCity;
     }
+  }
 
-    getPnrDestinations() {
-
-        if (this.isPNRLoaded) {
-            for (let air of this.pnrObj.airSegments) {
-                var airendpoint = air.arrivalAirport;
-                this.pushDestination(airendpoint)
-            }
-
-            for (let rm of this.pnrObj.miscSegments) {
-                // var endpoint = rm.fullNode.itineraryFreetext.boardpointDetail.cityCode;
-                var longFreetext = rm.fullNode.itineraryFreetext.longFreetext;
-                var endpoint = null
-                if (longFreetext.indexOf('/EC-') > -1) {
-                    var endpoint = longFreetext.substr(longFreetext.indexOf('/EC-') + 4, 3);
-                }
-                if (endpoint != null) {
-                    this.pushDestination(endpoint)
-                }
-            }
-            return this.destinationCity;
-
-
+  getPnrSegments() {
+    if (this.isPNRLoaded) {
+      for (const rm of this.pnrObj.get) {
+        if (rm.freeFlowText.indexOf("DE/-") === 0) {
+          return rm.elementNumber;
         }
-
+      }
     }
+    return "";
+  }
 
-    getPnrSegments() {
-        if (this.isPNRLoaded) {
-            for (const rm of this.pnrObj.get) {
-                if (rm.freeFlowText.indexOf('DE/-') === 0) {
-                    return rm.elementNumber;
-                }
-            }
-        }
-        return '';
+  getPassiveCarSegmentNumbers() {
+    const elementNumbers = new Array<number>();
+    for (const rm of this.pnrObj.auxCarSegments) {
+      elementNumbers.push(rm.elementNumber);
     }
+    return elementNumbers;
+  }
 
-    getPassiveCarSegmentNumbers() {
-        const elementNumbers = new Array<number>();
-        for (const rm of this.pnrObj.auxCarSegments) {
-            elementNumbers.push(rm.elementNumber);
-        }
-        return elementNumbers;
+  getPassiveHotelSegmentNumbers() {
+    const elementNumbers = new Array<number>();
+    for (const rm of this.pnrObj.auxHotelSegments) {
+      elementNumbers.push(rm.elementNumber);
     }
-
-    getPassiveHotelSegmentNumbers() {
-        const elementNumbers = new Array<number>();
-        for (const rm of this.pnrObj.auxHotelSegments) {
-            elementNumbers.push(rm.elementNumber);
-        }
-        return elementNumbers;
-    }
-
-
-
+    return elementNumbers;
+  }
 }

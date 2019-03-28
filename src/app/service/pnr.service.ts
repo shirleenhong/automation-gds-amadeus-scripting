6347cd7089532b71ceac79e593ee35e4195eb12a
Warrick Go <36612173+warrickgo@users.noreply.github.com>
2019-03-28 11:13:29 +0800

    Update pnr.service.ts

--- src/app/service/pnr.service.ts
+++ src/app/service/pnr.service.ts
@@ -27,7 +27,7 @@ export class PnrService {
         this.isPNRLoaded = false;
         this.errorMessage = 'Error:' + error;
       }
-    ).catch((err) => { console.log(err); });
+    ).catch(err => { console.log(err); });
     console.log(JSON.stringify(this.pnrObj));
   }
 

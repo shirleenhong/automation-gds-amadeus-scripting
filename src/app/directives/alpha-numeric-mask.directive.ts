a1f9ff2af2799871b1f65239ed56cd4ed9349a31
Philip <philip.monilla@carlsonwagonlit.com>
2019-03-29 14:54:38 +0800

    fix UI

---
+++ src/app/directives/alpha-numeric-mask.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
+
+@Directive({
+    selector: '[formControlName][AlphaNumericMask]',
+})
+export class AlphaNumericMaskDirective {
+    decPipe = new DecimalPipe('en-US');
+
+    constructor(public ngControl: NgControl) { }
+
+    @HostListener('ngModelChange', ['$event'])
+    onModelChange(event) {
+        this.onInputChange(event);
+    }
+
+
+
+    onInputChange(event) {
+        let newVal = event;
+
+        if (newVal === null) { return newVal; }
+        const lastChar = newVal.substr(newVal.length - 1);
+
+        if (lastChar.match(/[0-9]/g) === null && lastChar.match(/[a-z]/g) === null && lastChar.match(/[A-Z]/g) === null) {
+            newVal = newVal.substring(0, newVal.length - 1);
+        }
+        this.ngControl.valueAccessor.writeValue(newVal);
+    }
+}

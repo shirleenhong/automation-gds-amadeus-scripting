da44f12712eb640baf314ae4e5d733b3285e87f2
Philip <philip.monilla@carlsonwagonlit.com>
2019-04-05 13:31:40 +0800

    Update leisure.validators.ts

--- src/app/shared/validators/leisure.validators.ts
+++ src/app/shared/validators/leisure.validators.ts
@@ -1,1 +1,24 @@
+import { FormControl, Validators, ValidatorFn, FormGroup, ValidationErrors, AbstractControl } from '@angular/forms';
+import { SegmentService } from 'src/app/service/segment.service';
+import { isUndefined } from 'ngx-bootstrap/chronos/utils/type-checks';
+
+
+
+export function validateSegmentNumbers(segments: any[]): ValidatorFn {
+    return (currentControl: AbstractControl): { [key: string]: any } => {
+        if (currentControl.value === undefined) { return { no_value: true }; }
+        if (segments === undefined || segments.length === 0) { return { no_segments: true }; }
+        let response = null;
+        const segs = currentControl.value.toString().split(',');
+        segs.forEach(x => {
+            const s = segments.find(z => z.lineNo === x);
+            if (s == null || s === undefined) {
+                response = { segment_not_found: true };
+                return response;
+            }
+        });
+
+        return response;
+    };
+}
 

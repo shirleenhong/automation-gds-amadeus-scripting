7c7ea3e67226fdd565cd78d4f4591496c9d9cd4f
riza <rizalie.mallari@carlsonwagonlit.com>
2019-06-21 17:07:48 +0800

    Queue Change
    Queue Change

--- src/app/leisure/leisure.component.ts
+++ src/app/leisure/leisure.component.ts
@@ -159,6 +159,7 @@ export class LeisureComponent implements OnInit, AfterViewInit, AfterViewChecked
 
     const remarkCollection = new Array<RemarkGroup>();
     let queueCollection = Array<QueuePlaceModel>();
+    let itineraryQueueCollection = Array<QueuePlaceModel>();
 
     remarkCollection.push(this.paymentRemarkService.GetMatrixRemarks(this.paymentComponent.matrixReceipt.matrixReceipts));
     remarkCollection.push(this.paymentRemarkService.GetAccountingRemarks(this.paymentComponent.accountingRemark.accountingRemarks));
@@ -200,10 +201,10 @@ export class LeisureComponent implements OnInit, AfterViewInit, AfterViewChecked
     }
     if (!this.itineraryqueueComponent.itineraryComponent.itineraryForm.pristine) {
       remarkCollection.push(this.itineraryService.getItineraryRemarks(this.itineraryqueueComponent.itineraryComponent.itineraryForm));
-      queueCollection = this.itineraryService.addQueue(this.itineraryqueueComponent.queueComponent.queueForm);
-      const itireraryQueue = this.itineraryService.addItineraryQueue(this.itineraryqueueComponent.itineraryComponent.itineraryForm);
-      queueCollection = queueCollection.concat(itireraryQueue);
+      itineraryQueueCollection = this.itineraryService.addItineraryQueue(this.itineraryqueueComponent.itineraryComponent.itineraryForm);
     }
+    queueCollection = this.itineraryService.addQueue(this.itineraryqueueComponent.queueComponent.queueForm);
+    queueCollection = queueCollection.concat(itineraryQueueCollection);
 
     const leisureFee = this.paymentComponent.leisureFee;
     remarkCollection.push(this.paymentRemarkService.GetLeisureFeeRemarks(leisureFee, this.cfLine.cfa));

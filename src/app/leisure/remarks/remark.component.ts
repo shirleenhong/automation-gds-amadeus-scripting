855924a44c3a7e199d86d8df5a47a5c897fd8bcf
Don Cadavona <dcadavona@gmail.com>
2019-08-05 19:31:25 +0800

    DE2141: Fix Visa and Passport field requirements on PNR with International activity.

--- src/app/leisure/remarks/remark.component.ts
+++ src/app/leisure/remarks/remark.component.ts
@@ -104,6 +104,22 @@ export class RemarkComponent implements OnInit {
         return false;
       }
     }
+
+    // Check Visa and Passport
+    if (this.viewPassportComponent.isInternational
+        && (!this.viewPassportComponent.visaPassportView.citizenship.length
+            || !this.viewPassportComponent.visaPassportView.passportName.length
+            || !this.viewPassportComponent.isInternationalTravelAdvisorySent
+            || !this.viewPassportComponent.advisoryClicked)
+    ) {
+      // Indicate invalidities of the required Visa and Passport Advisory fields...
+      this.viewPassportComponent.visaPassportFormGroup.controls['isInternationalTravelAdvisorySent'].markAsTouched();
+      this.viewPassportComponent.visaPassportFormGroup.controls['citizenship'].markAsTouched();
+      this.viewPassportComponent.visaPassportFormGroup.controls['passportName'].markAsTouched();
+
+      return false;
+    }
+
     return true;
   }
 

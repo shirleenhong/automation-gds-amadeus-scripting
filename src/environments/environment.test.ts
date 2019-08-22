a9e2c9af1b25fd9e51323442a7e2f0a024055235
PhilipMon <47809074+PhilipMon@users.noreply.github.com>
2019-08-22 21:17:23 +0800

    apply - supplier code

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,5 @@
 export const environment = {
   proxy: false,
   production: false,
-  clientSecret: 'GYRgkW1JaJvKOXbQ5eQaaqcPT7O6aG2W',
-  remarksManagerUrlService: 'https://internal-test-int-cwt-bpg-lb-1345055561.us-west-2.elb.amazonaws.com'
+  clientSecret: 'GYRgkW1JaJvKOXbQ5eQaaqcPT7O6aG2W'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   proxy: true,
   production: false,
-  clientSecret: 'GYRgkW1JaJvKOXbQ5eQaaqcPT7O6aG2W',
-  remarksManagerUrlService: 'https://internal-test-int-cwt-bpg-lb-1345055561.us-west-2.elb.amazonaws.com'
+  clientSecret: 'GYRgkW1JaJvKOXbQ5eQaaqcPT7O6aG2W'
 };
 
 /*

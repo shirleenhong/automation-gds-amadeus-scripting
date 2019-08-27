4bc7f2ffb06bef2f6bbc223cd0186a7362a855b4
riza <rizalie.mallari@carlsonwagonlit.com>
2019-08-27 18:41:07 +0800

    Update aut.interceptors
    Update aut.interceptors

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -7,7 +7,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req, next): Observable<HttpEvent<any>> {
     let idToken = null;
 
-    if (req.url.indexAt('remarks-manager') > -1) {
+    if (req.url.indexOf('remarks-manager') > -1) {
       idToken = localStorage.getItem('token_rms');
     } else {
       idToken = localStorage.getItem('token');

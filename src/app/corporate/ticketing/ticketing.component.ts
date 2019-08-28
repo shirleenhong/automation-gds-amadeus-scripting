8c19d23363e84f53f3efebca72f8b6210fb31a4e
ron-cwt <40351310+ron-cwt@users.noreply.github.com>
2019-08-28 14:06:49 +0800

    US11386: Added draft fields for ticketing
    Draft fields for ticketing

--- src/app/corporate/ticketing/ticketing.component.ts
+++ src/app/corporate/ticketing/ticketing.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { StaticValuesService } from '../../service/static-values.services';
+import { SelectItem } from 'src/app/models/select-item.model';
 
 @Component({
-  selector: 'app-ticketing',
-  templateUrl: './ticketing.component.html',
-  styleUrls: ['./ticketing.component.scss']
+    selector: 'app-ticketing',
+    templateUrl: './ticketing.component.html',
+    styleUrls: ['./ticketing.component.scss']
 })
 export class TicketingComponent implements OnInit {
 
-  constructor() { }
+    tkList: Array<SelectItem> = null;
 
-  ngOnInit() {
-  }
+    constructor(private staticValues: StaticValuesService) { }
 
+    ngOnInit() {
+        this.loadTKList();
+    }
+
+    loadTKList() {
+        this.tkList = this.staticValues.getTKList();
+    }
 }

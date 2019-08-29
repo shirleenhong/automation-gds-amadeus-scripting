70b3c31dcfac79562a36fed9401c0fd5e8bcb9d8
ron-cwt <40351310+ron-cwt@users.noreply.github.com>
2019-08-29 19:49:54 +0800

    US11386: OID prepopulated on screen
    Added logic to extract OID from remarks to prepopulate textbox.

--- src/app/corporate/ticketing/ticketing.component.ts
+++ src/app/corporate/ticketing/ticketing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { StaticValuesService } from '../../service/static-values.services';
+import { PnrService } from '../../service/pnr.service';
 import { SelectItem } from 'src/app/models/select-item.model';
 
 @Component({
@@ -9,12 +11,48 @@ import { SelectItem } from 'src/app/models/select-item.model';
 })
 export class TicketingComponent implements OnInit {
 
+    ticketForm: FormGroup;
     tkList: Array<SelectItem> = null;
 
-    constructor(private staticValues: StaticValuesService) { }
+    constructor(private staticValues: StaticValuesService, private pnrService: PnrService) {
+        this.ticketForm = new FormGroup({
+            officeId: new FormControl('', [Validators.required]),
+            ticketDate: new FormControl('', [Validators.required]),
+            pnrOnHold: new FormControl('', []),
+            tk: new FormControl('', [Validators.required])
+        });
+
+    }
 
     ngOnInit() {
+        this.loadOid();
         this.loadTKList();
+
+    }
+
+    loadOid() {
+        const extractedOid = this.extractOidFromBookRemark();
+        this.ticketForm.get('officeId').setValue(extractedOid);
+    }
+
+    extractOidFromBookRemark() {
+        // const remarks = this.pnrService.getRemarksFromGDSByRegex(/BOOK-/g);
+        const BOOK_REMARK_PREFIX = 'BOOK-';
+        const TKT_PREFIX = 'TKT-';
+
+        const remarks = this.pnrService.getRemarkText(BOOK_REMARK_PREFIX);
+        let oid = null;
+
+        const remarkSplitted: Array<string> = remarks.split('/');
+        for (let i = 0; i < remarkSplitted.length; i++) {
+            let ctrRemarkSplit = remarkSplitted[i];
+            if (ctrRemarkSplit.match(TKT_PREFIX)) {
+                oid = ctrRemarkSplit.replace(TKT_PREFIX, '');
+                break;
+            }
+        }
+
+        return oid;
     }
 
     loadTKList() {

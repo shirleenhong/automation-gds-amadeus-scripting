092c9eb47351390f749a00bbcab18f14f83dfdea
Don Cadavona <dcadavona@gmail.com>
2019-09-18 16:13:45 +0800

    Fixed and run DB script by Riza.

--- db_script/sprint_3/US9402.sql
+++ db_script/sprint_3/US9402.sql
@@ -6,7 +6,6 @@ BEGIN TRY
 
 	DECLARE @CreationTimestamp		DATETIME = GETUTCDATE()	
 	DECLARE @CreationUserIdentifier NVARCHAR(170)
-	
 	DECLARE @PNROutputItemId AS INT
 	DECLARE @PNROutputGroupID AS INT
 
@@ -18,6 +17,7 @@ BEGIN TRY
 	DELETE FROM PNROutputItem WHERE CreationUserIdentifier = @CreationUserIdentifier
 	DELETE FROM PNROutputPlaceHolder WHERE	CreationUserIdentifier = @CreationUserIdentifier
 	DELETE FROM ConfigurationParameter WHERE CreationUserIdentifier = @CreationUserIdentifier
+	DELETE FROM Context WHERE CreationUserIdentifier = @CreationUserIdentifier
 
 	----------------------------------
 	-- Insert Scripts
@@ -26,30 +26,30 @@ BEGIN TRY
 
 		SET @CreationUserIdentifier			= 'Amadeus CA Migration - US9402'	
 		SET @PNROutputItemId =	(SELECT MAX(PNROutputItemId)  FROM [PNROutputItem])
-		SET @PNROutputGroupID =	(SELECT MAX(PNROutputGroupId)  FROM [PNROutputGroup])
-		SET @PNROutputRemarkGroupId = (SELECT MAX(PNROutputRemarkGroupId)  FROM PNROutputRemarkGroup)
-
+		SET @PNROutputGroupID =	(SELECT PNROutputGroupId  FROM [PNROutputGroup] where pnroutputgroupname = 'Canada Migration General Group')
+		
 		-- Configs
 		INSERT INTO Context (ContextId, ContextName, CreationTimestamp,CreationUserIdentifier, VersionNumber)
 			Values(11, 'Amadeus Corp Scripting',  GETUTCDATE()	, 'Amadeus CA Migration - US9402', 1)
 		INSERT INTO ConfigurationParameter(ConfigurationParameterName, ConfigurationParameterValue, ContextId, CreationTimestamp, CreationUserIdentifier, VersionNumber)
 			VALUES ('MigrationOBTFee ', 'RBP, NRD, ABC', 11, GETUTCDATE()	, 'Amadeus CA Migration - US9402', 1)
 		INSERT INTO ConfigurationParameter(ConfigurationParameterName, ConfigurationParameterValue, ContextId, CreationTimestamp, CreationUserIdentifier, VersionNumber)
-			VALUES ('MigrationOBTFeeDate ', '01/30/2019,11/01/2019', 11, GETUTCDATE()	, 'Amadeus CA Migration - US9402', 1),
-		INSERT INTO PNROutputGroupPNROutputItem (PNROutputGroupId, PNROutputItemId, SequenceNumber, CreationTimestamp, CreationUserIdentifier, VersionNumber, DataStandardizationVersion, LayoutVersion)
-			VALUES (@PNROutputGroupID,  @PNROutputItemID + 1, 0, @CreationTimeStamp, @CreationUserIdentifier, 1, 1, 1)
+			VALUES ('MigrationOBTFeeDate ', '01/30/2019,11/01/2019', 11, GETUTCDATE()	, 'Amadeus CA Migration - US9402', 1)
 
 		-- Remarks
-		-- doing...
 		-- If air is being processed: RMF/SUPFEE1-ATE
 		-- If rail is being processed: RMF/SUPFEE1-RTE
 		-- If hotel is being processed: RMF/SUPFEE1-HBE
 		-- If car is being processed: RMF/SUPFEE1-CBE
+		INSERT INTO [dbo].[PNROutputPlaceHolder]([PNROutputPlaceHolderName],[PNROutputPlaceHolderRegularExpresssion],[CreationTimestamp],[CreationUserIdentifier],[VersionNumber])
+									VALUES( '%SupfeeSegment%', '(ATE|RTE|HBE|CBE)',@CreationTimestamp, @CreationUserIdentifier, 1 )
+
 		INSERT INTO [dbo].[PNROutputItem]([PNROutputItemId],[PNROutputRemarkTypeCode],[PNROutputBindingTypeCode],[PNROutputUpdateTypeCode],[GDSRemarkQualifier],[RemarkFormat],[CreationTimestamp],[CreationUserIdentifier],[VersionNumber],[PNROutputItemDefaultLanguageCode],[PNROutputItemXMLFormat])
-			VALUES 	(@PNROutputItemId + 1,3,'0',1,'','RMF/SUPFEE1-ATE', @CreationTimestamp,@CreationUserIdentifier,1,'en-GB',NULL),
-					(@PNROutputItemId + 2,3,'0',1,'','RMF/SUPFEE1-RTE', @CreationTimestamp,@CreationUserIdentifier,1,'en-GB',NULL),
-					(@PNROutputItemId + 3,3,'0',1,'','RMF/SUPFEE1-HBE', @CreationTimestamp,@CreationUserIdentifier,1,'en-GB',NULL),
-					(@PNROutputItemId + 4,3,'0',1,'','RMF/SUPFEE1-CBE', @CreationTimestamp,@CreationUserIdentifier,1,'en-GB',NULL)
+			VALUES 	(@PNROutputItemId + 1,0,'0',1,'','RMF/SUPFEE1-%SupfeeSegment%', @CreationTimestamp,@CreationUserIdentifier,1,'en-GB',NULL)
+		
+
+		INSERT INTO PNROutputGroupPNROutputItem (PNROutputGroupId, PNROutputItemId, SequenceNumber, CreationTimestamp, CreationUserIdentifier, VersionNumber, DataStandardizationVersion, LayoutVersion)
+			VALUES (@PNROutputGroupID,  @PNROutputItemID + 1, 0, @CreationTimeStamp, @CreationUserIdentifier, 1, 1, 1)
 
 	PRINT 'END Script'
 

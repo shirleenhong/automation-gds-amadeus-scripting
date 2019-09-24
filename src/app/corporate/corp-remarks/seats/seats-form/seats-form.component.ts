b31916869487ef62f60b9fe1810a8bf96f4ddba3
Don Cadavona <dcadavona@gmail.com>
2019-09-24 19:50:09 +0800

    Initial seat form validations and conditions.

--- src/app/corporate/corp-remarks/seats/seats-form/seats-form.component.ts
+++ src/app/corporate/corp-remarks/seats/seats-form/seats-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
 import { SeatModel } from 'src/app/models/pnr/seat.model';
 import { SeatsService } from 'src/app/service/corporate/seats.service';
 
@@ -13,7 +13,13 @@ export class SeatsFormComponent implements OnInit {
 
   @Input()
 
+  REGEX_ALPHANUMERIC = '\w';
+
   seat: SeatModel;
+  text: string;
+  number: string = null;
+  segmentId: string;
+
   remarkOptions: Array<string>;
   types: Array<string>;
   segments: Array<any>;
@@ -32,10 +38,13 @@ export class SeatsFormComponent implements OnInit {
     this.segments      = this.getSegments();
 
     this.seatsFormGroup = new FormGroup({
-      type: new FormControl(),
-      number: new FormControl(),
-      text: new FormControl(),
+      text: new FormControl('', [ Validators.required ]),
+      type: new FormControl({ value: '', disabled: true }, [ Validators.required ]),
+      number: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.pattern(this.REGEX_ALPHANUMERIC) ]),
+      segmentId: new FormControl('', [ Validators.required ]),
     });
+
+    this.onChanges();
   }
 
   save(): void {
@@ -60,5 +69,52 @@ export class SeatsFormComponent implements OnInit {
         name: 'Segment 3'
       }
     ];
+  }
+
+  /**
+   * Handle changes on the seat form.
+   */
+  public onChanges(): void {
+
+    this.seatsFormGroup.valueChanges.subscribe(() => {
+      console.log('CHANGED this.seatsFormGroup. Invalid: ' + this.seatsFormGroup.invalid);
+      console.log(JSON.stringify(this.seatsFormGroup.value));
+
+      Object.keys(this.seatsFormGroup.controls).forEach(key => {
+        const controlErrors: ValidationErrors = this.seatsFormGroup.get(key).errors;
+        if (controlErrors != null) {
+          Object.keys(controlErrors).forEach(keyError => {
+            console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+          });
+        }
+      });
+    });
+
+    // Disable or enable the type and number form controls based on type.
+    this.seatsFormGroup.get('text').valueChanges.subscribe((value) => {
+
+      console.log('Value: ' + value);
+      console.log('Text: ' + this.remarkOptions.indexOf(value) + 1);
+
+      switch (this.remarkOptions.indexOf(value) + 1) {
+        case 2:
+          console.log('Enabling type...');
+          this.seatsFormGroup.get('type').enable();
+          this.seatsFormGroup.get('type').setValidators([Validators.required]);
+          break;
+        case 5:
+          console.log('Enabling type and number...');
+          this.seatsFormGroup.get('type').enable();
+          this.seatsFormGroup.get('type').setValidators([Validators.required]);
+          this.seatsFormGroup.get('number').enable();
+          this.seatsFormGroup.get('number').setValidators([Validators.required]);
+          break;
+        default:
+          console.log('Disabling type and number...');
+          this.seatsFormGroup.get('type').disable();
+          this.seatsFormGroup.get('number').disable();
+          break;
+      }
+    });
   }
 }

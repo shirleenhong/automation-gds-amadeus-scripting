d4eb3c85ec4d6960720c967c3dc4a1cfe97724a9
MicoPerez <47840562+MicoPerez@users.noreply.github.com>
2019-09-26 18:54:46 +0800

    Writing Remarks
    Writing Remarks

--- src/app/corp-remarks.service.ts
+++ src/app/corp-remarks.service.ts
@@ -6,8 +6,7 @@ import { SeatModel } from 'src/app/models/pnr/seat.model';
   providedIn: 'root'
 })
 export class CorpRemarksService {
-
-  constructor(private remarksManagerService: RemarksManagerService) { }
+  constructor(private remarksManagerService: RemarksManagerService) {}
 
   /**
    * Write or prepare the seats for the PNR
@@ -22,16 +21,56 @@ export class CorpRemarksService {
 
       if (seat) {
         const segments = seat.segmentId.split(',');
+        const seatMap = new Map<string, string>();
+        const seatType = new Map<string, string>();
 
+        seatType.set('CaSeatType', seat.type);
         for (let segmentsCounter = 0; segmentsCounter <= segments.length; segmentsCounter++) {
           const segment = segments[segmentsCounter];
 
-          const seatMap = new Map<string, string>();
-          seatMap.set('ONLINECHECKIN', 'true');
-          this.remarksManagerService.createPlaceholderValues(seatMap, null, new Array(segment));
+          if (seat.text === 'SEATING SUBJECT TO AIRPORT OR ONLINE CHECK IN') {
+            //
+            seatMap.set('CASeatRule', 'ONLINECHECKIN');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'SEATING SUBJECT TO');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'AIRPORT OR ONLINE CHECK IN');
+            //
+          } else if (seat.text === 'PREFERRED SEAT UNAVAILABLE. SEAT TYPE CONFIRMED') {
+            //
+            seatMap.set('CASeatRule', 'PREFERRED');
+            if (seat.type !== '') {
+              this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'PREFERRED SEAT UNAVAILABLE');
+            } else {
+              this.remarksManagerService.createPlaceholderValues(seatType, null);
+            }
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'PLEASE CHECK AGAIN AT THE GATE');
+            //
+          } else if (seat.text === 'THIS SEGMENT HAS BEEN WAITLIST') {
+            //
+            seatMap.set('CASeatRule', 'WAITLISTED');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'SEATING SUBJECT TO');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'AIRPORT OR ONLINE CHECK IN');
+            //
+          } else if (seat.text === 'SEAT ASSIGNMENTS ARE ON REQUEST') {
+            //
+            seatMap.set('CASeatRule', 'REQUEST');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'SEATING SUBJECT TO');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'AIRPORT OR ONLINE CHECK IN');
+            //
+          } else if (seat.text === 'UPGRADE CONFIRMED - SEAT NUMBER CONFIRMED') {
+            //
+            seatMap.set('CASeatRule', 'CONFIRMED');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'SEATING SUBJECT TO');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'AIRPORT OR ONLINE CHECK IN');
+            //
+          } else if (seat.text === 'UPGRADE REQUESTED - CHECK CLEARANCE WITH AIRLINE OR AIRLINE WEBSITE') {
+            //
+            seatMap.set('CASeatRule', 'CLEARANCE');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'SEATING SUBJECT TO');
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'AIRPORT OR ONLINE CHECK IN');
+            //
+          }
         }
       }
-
     }
   }
 }
--- src/app/service/corporate/corp-remarks.service.ts
+++ src/app/service/corporate/corp-remarks.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { RemarksManagerService } from 'src/app/service/corporate/remarks-manager.service';
 import { SeatModel } from 'src/app/models/pnr/seat.model';
+import { PnrService } from '../pnr.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CorpRemarksService {
-  constructor(private remarksManagerService: RemarksManagerService) {}
+  constructor(private remarksManagerService: RemarksManagerService, private pms: PnrService) {}
 
   /**
    * WIP
@@ -22,24 +23,67 @@ export class CorpRemarksService {
     seats.forEach((seat) => {
       debugger;
       // Condition 1
-      if (seat.segmentId) {
-        debugger;
-        const segments = seat.segmentId.split(',');
 
-        segments.forEach((segment) => {
-          const seatMap = new Map<string, string>();
-          // WIP
+      debugger;
+      const segments = seat.segmentId.split(',');
+
+      segments.forEach((segment) => {
+        const seatMap = new Map<string, string>();
+        // WIP
+        const tatooNumber = this.pms.getTatooNumberFromSegmentNumber(new Array(segment));
+        debugger;
+        if (seat.text === 'SEATING SUBJECT TO AIRPORT OR ONLINE CHECK IN') {
+          //
           seatMap.set('CASeatRule', 'ONLINECHECKIN');
           this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'SEATING SUBJECT TO');
-          this.remarksManagerService.createPlaceholderValues(null, seatMap, new Array(segment), null, 'AIRPORT OR ONLINE CHECK IN');
-        });
-      }
+          // tslint:disable-next-line: max-line-length
+          this.remarksManagerService.createPlaceholderValues(null, seatMap, tatooNumber, null, 'AIRPORT OR ONLINE CHECK IN');
+          //
+        } else if (seat.text === 'PREFERRED SEAT UNAVAILABLE. SEAT TYPE CONFIRMED') {
+          //
+          seatMap.set('CASeatRule', 'PREFERRED');
+          if (seat.type !== '') {
+            this.remarksManagerService.createPlaceholderValues(null, seatMap, tatooNumber, null, 'PREFERRED SEAT UNAVAILABLE');
+          } else {
+            const seatType = new Map<string, string>();
+            seatType.set('CaSeatType', seat.type);
+            this.remarksManagerService.createPlaceholderValues(seatType, null);
+          }
 
-      // Condition 2...
-
-      // Condition 3...
-
-      // Condition 4 and so on...
+          this.remarksManagerService.createPlaceholderValues(null, seatMap, tatooNumber, null, 'PLEASE CHECK AGAIN AT THE GATE');
+          //
+        } else if (seat.text === 'THIS SEGMENT HAS BEEN WAITLIST') {
+          //
+          seatMap.set('CASeatRule', 'WAITLISTED');
+          this.remarksManagerService.createPlaceholderValues(null, seatMap, tatooNumber, null, 'THIS SEGMENT HAS BEEN WAITLISTED');
+          //
+        } else if (seat.text === 'SEAT ASSIGNMENTS ARE ON REQUEST') {
+          //
+          seatMap.set('CASeatRule', 'REQUEST');
+          this.remarksManagerService.createPlaceholderValues(null, seatMap, tatooNumber, null, 'SEAT ASSIGNMENTS ARE ON REQUEST');
+          //
+        } else if (seat.text === 'UPGRADE CONFIRMED - SEAT NUMBER CONFIRMED') {
+          //
+          seatMap.set('CASeatRule', 'CONFIRMED');
+          const seatConfirmed = new Map<string, string>();
+          seatConfirmed.set('CaUPFIB', seat.number);
+          this.remarksManagerService.createPlaceholderValues(seatConfirmed, null);
+          this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'UPGRADE CONFIRMED');
+          //
+        } else if (seat.text === 'UPGRADE REQUESTED - CHECK CLEARANCE WITH AIRLINE OR AIRLINE WEBSITE') {
+          //
+          seatMap.set('CASeatRule', 'CLEARANCE');
+          this.remarksManagerService.createPlaceholderValues(null, seatMap, null, null, 'UPGRADE REQUESTED');
+          this.remarksManagerService.createPlaceholderValues(
+            null,
+            seatMap,
+            tatooNumber,
+            null,
+            'CHK CLEARANCE WITH AIRLINE OR AIRLINE WEBSITE'
+          );
+          //
+        }
+      });
     });
   }
 }

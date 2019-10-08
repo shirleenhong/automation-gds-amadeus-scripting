98fa9e7edcd2b874143459a54f06f5ba6c5a4eef
PhilipMon <Philip.monilla@carlsonwagonlit.com>
2019-10-08 17:37:44 +0800

    apply fix

---
+++ src/app/models/validate-model.ts
@@ -0,0 +1,50 @@
+export class ValidateModel {
+  isPaymentValid = false;
+  isReportingValid = false;
+  isRemarkValid = false;
+  isSegmentValid = false;
+  isSubmitted = false;
+  isFeesValid = false;
+  isQueueValid = false;
+  // isItineraryValid = false;
+  isTicketingValid = false;
+  paymentValidCss = '';
+  reportingValidCss = '';
+  remarkValidCss = '';
+  segmentValidCss = '';
+  itineraryValidCss = '';
+  ticketingValidCss = '';
+  feesValidCss = '';
+  queueValidCss = '';
+
+  setCssClass() {
+    this.paymentValidCss = this.getCss(this.isPaymentValid);
+    this.reportingValidCss = this.getCss(this.isReportingValid);
+    this.remarkValidCss = this.getCss(this.isRemarkValid);
+    this.segmentValidCss = this.getCss(this.isSegmentValid);
+    this.ticketingValidCss = this.getCss(this.isTicketingValid);
+    this.feesValidCss = this.getCss(this.isFeesValid);
+    this.queueValidCss = this.getCss(this.isQueueValid);
+    // this.itineraryValidCss = this.getCss(this.isItineraryValid);
+  }
+
+  getCss(val: boolean) {
+    if (this.isSubmitted) {
+      return val ? 'fas fa-check checkValid' : 'fas fa-times crossInvalid';
+    } else {
+      return '';
+    }
+  }
+
+  isAllValid() {
+    this.setCssClass();
+    return this.isPaymentValid && this.isReportingValid && this.isRemarkValid;
+  }
+
+  isCorporateAllValid() {
+    this.setCssClass();
+    return (
+      this.isPaymentValid && this.isReportingValid && this.isTicketingValid && this.isFeesValid && this.isRemarkValid && this.isQueueValid
+    );
+  }
+}

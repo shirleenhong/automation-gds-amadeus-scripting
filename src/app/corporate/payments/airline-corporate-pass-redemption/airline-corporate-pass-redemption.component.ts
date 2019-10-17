2d4fd45b3bff924aad52ae0c2e922495a40c245e
Don Cadavona <dcadavona@gmail.com>
2019-10-17 16:34:55 +0800

    Initial component generated.

---
+++ src/app/corporate/payments/airline-corporate-pass-redemption/airline-corporate-pass-redemption.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-airline-corporate-pass-redemption',
+  templateUrl: './airline-corporate-pass-redemption.component.html',
+  styleUrls: ['./airline-corporate-pass-redemption.component.scss']
+})
+export class AirlineCorporatePassRedemptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

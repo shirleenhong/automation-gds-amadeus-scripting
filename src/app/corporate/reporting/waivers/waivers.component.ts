74ed31b25b07b8d051ea1d93266229efd971c15a
riza <rizalie.mallari@carlsonwagonlit.com>
2020-02-12 17:51:31 +0800

    Fixes
    Fixes

--- src/app/corporate/reporting/waivers/waivers.component.ts
+++ src/app/corporate/reporting/waivers/waivers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PnrService } from 'src/app/service/pnr.service';
-import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { AddWaiverComponent } from 'src/app/corporate/reporting/waivers/add-waiver/add-waiver.component';
 
@@ -16,59 +16,35 @@ export class WaiversComponent implements OnInit {
   selectedGroup: FormGroup;
 
   constructor(private pnrService: PnrService, private fb: FormBuilder, private modalService: BsModalService) {
-    this.modalSubscribeOnClose();
     this.ticketedForm = this.fb.group({
-      segments: this.fb.array([])
+      waiver: new FormControl()
     });
   }
 
   async ngOnInit() {
-    await this.loadData();
-    this.ticketedForm = this.fb.group({
-      segments: this.fb.array([])
-    });
-
-    this.ticketedSegments = await this.pnrService.getTstSegments();
-    for (const segment of this.ticketedSegments) {
-      const group = this.createFormGroup(segment);
-      (this.ticketedForm.get('segments') as FormArray).push(group);
-    }
   }
 
-  getWaiverValue(segmentNo): string {
+  getWaiverValue(): string {
     let val: string;
 
     for (const element of this.pnrService.pnrObj.rmElements) {
       if (element.freeFlowText.includes('U63')) {
-        const tatoo = this.pnrService.getTatooNumberFromSegmentNumber(segmentNo);
-        if (tatoo.includes(element.fullNode.referenceForDataElement.reference.number)) {
-          val = element.freeFlowText.split('U63/-')[1];
-          return val;
-        }
+        val = element.freeFlowText.split('U63/-')[1];
+        return val;
       }
     }
   }
 
-  async loadData(): Promise<void> { }
-  createFormGroup(segmentNo) {
-    const val = this.getWaiverValue(segmentNo);
-    return this.fb.group({
-      segment: new FormControl(segmentNo),
-      waiver: new FormControl(val)
-    });
-  }
 
   modalSubscribeOnClose() {
     this.modalService.onHide.subscribe(() => {
-      if (this.selectedGroup) {
-        this.waiverChange(this.selectedGroup);
-      }
+      this.waiverChange();
     });
   }
 
-  waiverChange(group: FormGroup) {
-    const waiver = group.get('waiver');
-    if (group.get('code').value === '') {
+  waiverChange() {
+    const waiver = this.ticketedForm.get('waiver');
+    if (this.ticketedForm.get('code').value === '') {
       waiver.setValidators([Validators.required]);
     } else {
       waiver.setValidators(null);
@@ -98,22 +74,22 @@ export class WaiversComponent implements OnInit {
     }
   }
 
-  removeWaivers(item) {
-    const waiver = item.controls.waiver.value;
+  removeWaivers() {
+    const waiver = this.ticketedForm.get('waiver').value;
     const arr = waiver.split('/');
     const len = arr.length;
 
     if (len === 3) {
-      item.controls.waiver.setValue(arr[0] + '/' + arr[1]);
+      this.ticketedForm.get('waiver').setValue(arr[0] + '/' + arr[1]);
     } else if (len === 2) {
-      item.controls.waiver.setValue(arr[0]);
+      this.ticketedForm.get('waiver').setValue(arr[0]);
     } else {
-      item.controls.waiver.setValue('');
+      this.ticketedForm.get('waiver').setValue('');
     }
   }
 
-  addWaivers(group) {
-    const waiver = group.get('waiver');
+  addWaivers() {
+    const waiver = this.ticketedForm.get('waiver');
     if (waiver !== undefined) {
       const val = waiver.value;
       if (val !== null) {
@@ -123,14 +99,14 @@ export class WaiversComponent implements OnInit {
             backdrop: 'static'
           });
           this.modalRef.content.title = 'Add Waiver / Favor';
-          this.modalRef.content.setWaiverItem(group.get('waiver'));
+          this.modalRef.content.setWaiverItem(this.ticketedForm.get('waiver'));
         }
       } else {
         this.modalRef = this.modalService.show(AddWaiverComponent, {
           backdrop: 'static'
         });
         this.modalRef.content.title = 'Add Waiver / Favor';
-        this.modalRef.content.setWaiverItem(group.get('waiver'));
+        this.modalRef.content.setWaiverItem(this.ticketedForm.get('waiver'));
       }
     }
   }

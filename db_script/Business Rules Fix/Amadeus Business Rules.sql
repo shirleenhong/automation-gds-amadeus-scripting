9d6ebff3890d11cd082d3003de982ed56aa652eb
MicoPerez <47840562+MicoPerez@users.noreply.github.com>
2020-03-04 17:22:42 +0800

    fix for script

--- db_script/Business Rules Fix/Amadeus Business Rules.sql
+++ db_script/Business Rules Fix/Amadeus Business Rules.sql
@@ -16089,7 +16089,7 @@ declare @CONTAINS as int
 declare @NOTCONTAINS as int
 DECLARE @CFA as varchar(5) = 'NZ7'
 
-set @CreationUserIdentifier = 'Amadeus CA Migration  ' + @CFA +  ' - US15251'
+set @CreationUserIdentifier = 'CA Migration  ' + @CFA +  ' - US15251'
 set @CDRGRoupName = 'Amadeus CA Migration - ' + @CFA +  ' Rule 1'
 
 set @ClientSubUnitGuid = 'A:FA177'
@@ -16222,9 +16222,9 @@ VALUES
 
     ( @CDRGRoupName, @bid2, 'REPORTING', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
     ( @CDRGRoupName, @bid3, '{"type":"text","label":"YUP Fare","name":"yupFare_[TSTSEGMENT]","required":"false","valuetype":"AmountMask"}', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
-	( @CDRGRoupName, @bid6, 'RM* U3/-[UI_FORM_yupFare_TSTSEGMENT]', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
-	( @CDRGRoupName, @bid7, 'RMT TKT[TSTNumber]-U3/-[UI_FORM_yupFare_TSTSEGMENT]', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
-	( @CDRGRoupName, @bid5, 'U3/-', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+    ( @CDRGRoupName, @bid6, 'RM* U3/-[UI_FORM_yupFare_TSTSEGMENT]', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    ( @CDRGRoupName, @bid7, 'RMT TKT[TSTNumber]-U3/-[UI_FORM_yupFare_TSTSEGMENT]', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    ( @CDRGRoupName, @bid5, 'U3/-', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
     
 
 SET @resultitemid = SCOPE_IDENTITY() - 5; -- count of records
@@ -16236,8 +16236,8 @@ values
     (@resultitemid + 1, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
     (@resultitemid + 2, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
     (@resultitemid + 3, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
-	(@resultitemid + 4, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
-	(@resultitemid + 5, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+    (@resultitemid + 4, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    (@resultitemid + 5, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
 	
 
 INSERT INTO [ClientDefinedRuleGroupClientAccount]
@@ -16259,6 +16259,205 @@ RAISERROR(@ErrorMessage, 10, 1);
 END CATCH
 GO
 ------------
+
+BEGIN TRAN
+BEGIN TRY
+DECLARE @CreationUserIdentifier nvarchar(200)
+DECLARE @CreationTimestamp DATETIME = GETUTCDATE()
+DECLARE @ClientSubUnitGuid as varchar(50)
+DECLARE @ClientAccountGuid as varchar(50)
+DECLARE @ClientTopUnitGuid as  varchar(50)
+DECLARE @TravellerTypeGuid as varchar(50)
+DECLARE @SourceSystemCode as varchar(50)
+DECLARE @NextGroupSequenceNumber as int
+DECLARE @CDRGroupName  as varchar(300)
+DECLARE @CDRGId as int
+DECLARE @WFId_WS_NB as int
+DECLARE @WFId_WS_AB as int
+DECLARE @WFId_RB_NB as int
+DECLARE @WFId_RB_AB as int
+DECLARE @WFId_DY_NB as int
+DECLARE @WFId_DY_AB as int
+DECLARE @WFId_WFPR_NB as int
+DECLARE @WFId_WFP_NB as int
+declare @WFId_WFUPR_NB as int
+declare @WFId_WFUP_NB as int 
+DECLARE @AnyTripType as int
+DECLARE @logicitemid AS int
+DECLARE @resultitemid AS int
+declare @shouldmaplogic as bit
+declare @shouldmapresult as bit
+Declare @WFId_RBPR_NB as int
+Declare @WFId_RBP_NB as int
+Declare @WFId_RBPR_AB as int
+Declare @WFId_RBP_AB as int
+DECLARE @WFId_WFPR_AB as int
+DECLARE @WFId_WFP_AB as int
+DECLARE @CORP_LOAD_FULLWRAP as int
+
+DECLARE @IS AS int
+declare @CONTAINS as int
+declare @IN as int
+DECLARE @CFA as varchar(5) = 'SGE'
+
+set @CreationUserIdentifier = 'CA Migration  ' + @CFA +  ' - US15070'
+set @CDRGRoupName = 'Amadeus CA Migration - ' + @CFA +  ' Rule 2'
+
+set @ClientSubUnitGuid = 'A:FA177'
+set @TravellerTypeGuid = ''
+set @ClientAccountGuid=''
+set @SourceSystemCode = 'CA1'
+set @ClientTopUnitGuid = ''
+SELECT @NextGroupSequenceNumber =1 --- isnull(max(GroupSequenceNumber)+1,1) FROM ClientDefinedRuleGroup WHERE CreationUserIdentifier = @CreationUserIdentifier
+
+
+---- ROLLBACK
+DELETE FROM ClientDefinedRuleGroupClientAccount where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleGroupResult where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleResultItem where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleGroupLogic where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleLogicItem where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleGroupTrigger where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleGroupClientSubUnit where CreationUserIdentifier= @CreationUserIdentifier
+DELETE FROM ClientDefinedRuleGroup where CreationUserIdentifier= @CreationUserIdentifier
+
+
+--dbo.ClientDefinedRuleGroup
+INSERT INTO ClientDefinedRuleGroup
+    ( TripTypeId,ClientDefinedRuleGroupName,CreationTimeStamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber,ClientDefinedRuleGroupDescription,EnabledFlag,EnabledDate,ExpiryDate,DeletedFlag,DeletedDateTime,Category)
+VALUES
+    ( null, @CDRGRoupName, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1, @CDRGRoupName, 1, '01/01/2016', null, 0, null, null)
+
+
+SELECT @CDRGId=ClientDefinedRuleGroupID
+FROM dbo.ClientDefinedRuleGroup
+WHERE ClientDefinedRuleGroupName = @CDRGRoupName
+SELECT @CORP_LOAD_FULLWRAP= ClientDefinedRuleWorkflowTriggerID
+FROM ClientDefinedRuleWorkflowTrigger
+ wt INNER JOIN
+    (SELECT ClientDefinedRuleWorkflowTriggerStateID, ClientDefinedRuleWorkflowTriggerStateName, ClientDefinedRuleWorkflowTriggerApplicationModeID, ClientDefinedRuleWorkflowTriggerApplicationModeName
+    from ClientDefinedRuleWorkflowTriggerState
+  CROSS JOIN  ClientDefinedRuleWorkflowTriggerApplicationMode) wt2
+    ON wt.ClientDefinedRuleWorkflowTriggerStateID = wt2.ClientDefinedRuleWorkflowTriggerStateID
+        AND wt.ClientDefinedRuleWorkflowTriggerApplicationModeID = wt2.ClientDefinedRuleWorkflowTriggerApplicationModeID
+WHERE wt2.ClientDefinedRuleWorkflowTriggerStateName='CORPLoadPnr' AND
+    wt2.ClientDefinedRuleWorkflowTriggerApplicationModeName='CORPFullWrap'
+
+SELECT @IS = ClientDefinedRuleRelationalOperatorid
+FROM ClientDefinedRuleRelationalOperator
+where RelationalOperatorName = 'IS'
+SELECT @CONTAINS = ClientDefinedRuleRelationalOperatorid
+FROM ClientDefinedRuleRelationalOperator
+where RelationalOperatorName = 'CONTAINS'
+SELECT @IN = ClientDefinedRuleRelationalOperatorid
+FROM ClientDefinedRuleRelationalOperator
+where RelationalOperatorName = 'IN'
+
+
+--ClientDefinedRuleGroupTrigger
+INSERT INTO dbo.ClientDefinedRuleGroupTrigger
+    ( ClientDefinedRuleGroupId,ClientDefinedRuleWorkflowTriggerId,CreationTimestamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber)
+VALUES
+    ( @CDRGId, @CORP_LOAD_FULLWRAP, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+
+DECLARE  @bid as int 
+DECLARE  @bid2 as int 
+DECLARE  @bid3 as int 
+DECLARE  @bid4 as int
+DECLARE  @bid5 as int
+
+--ClientDefinedRuleLogicItem
+SET @bid=null; 
+SELECT @bid=ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='PNR_CF';  SET @logicitemid = null; 
+
+SET @bid2=null; 
+SELECT @bid2=ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='PNR_SEGMENT_TYPES_IN_PNR';  
+
+SET @bid3 =null; 
+SELECT @bid3 =ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='PNR_AS_ClassOfService'; 
+
+
+SET @logicitemid = null; 
+
+
+    INSERT INTO dbo.ClientDefinedRuleLogicItem
+    ( ClientDefinedRuleLogicItemDescription,ClientDefinedRuleBusinessEntityId,ClientDefinedRuleRelationalOperatorId,ClientDefinedRuleLogicItemValue,CreationTimestamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber)
+VALUES
+    ( @CDRGRoupName, @bid, @IS, @CFA, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    ( @CDRGRoupName, @bid2, @CONTAINS, 'AIR', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+  
+    SET @logicitemid = SCOPE_IDENTITY() - 3
+
+
+    INSERT INTO dbo.ClientDefinedRuleGroupLogic
+    (ClientDefinedRuleLogicItemId, ClientDefinedRuleGroupId, LogicSequenceNumber, CreationTimestamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber)
+values
+    (@logicitemid + 1, @CDRGId, 1 , @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    (@logicitemid + 2, @CDRGId, 2 , @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+
+
+SELECT @bid2 = ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='UI_DISPLAY_CONTAINER'; 
+
+SELECT @bid3 =  ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='PNR_DELETE_APE_REMARKS'; 
+
+SELECT @bid4 = ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='UI_ADD_CONTROL'; 
+
+SELECT @bid5 =  ClientDefinedRuleBusinessEntityID
+FROM ClientDefinedRuleBusinessEntity
+WHERE BusinessEntityName='PNR_WRITE_REMARK_WITH_CONDTION'; 
+
+
+    INSERT INTO dbo.ClientDefinedRuleResultItem
+    ( ClientDefinedRuleResultItemDescription,ClientDefinedRuleBusinessEntityId,ClientDefinedRuleResultItemValue,CreationTimestamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber)
+VALUES
+
+    ( @CDRGRoupName, @bid2, 'QUEUE', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    ( @CDRGRoupName, @bid3, 'APE SELMA.MAAREF@SGCIB.COM', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    ( @CDRGRoupName, @bid4, '{"type":"select","label":"Is Business Class Booked?","name":"isBusiness","required":"true","options":[{"name":"Yes","value":"Yes"},{"name":"No","value":"No"}]}', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    ( @CDRGRoupName, @bid5, '{"conditions":[{"controlName":"isBusiness","propertyName":"","operator":"IS","value":"Yes","segmentType":""}],"remarks":["APE SELMA.MAAREF@SGCIB.COM"]}', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+
+
+SET @resultitemid = SCOPE_IDENTITY() - 4; -- count of records
+
+    INSERT INTO dbo.ClientDefinedRuleGroupResult
+    (ClientDefinedRuleResultItemId, ClientDefinedRuleGroupId, CreationTimestamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber)
+values
+    (@resultitemid + 1, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    (@resultitemid + 2, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    (@resultitemid + 3, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1),
+    (@resultitemid + 4, @CDRGId, @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+	
+
+
+INSERT INTO [ClientDefinedRuleGroupClientAccount]
+    (ClientDefinedRuleGroupId, ClientAccountNumber, SourceSystemCode, CreationTimestamp,CreationUserIdentifier,LastUpdateTimeStamp,LastUpdateUserIdentifier,VersionNumber)
+VALUES
+    (@CDRGId, '1' + @CFA, 'CA1', @CreationTimestamp, @CreationUserIdentifier, @CreationTimestamp, @CreationUserIdentifier, 1)
+
+
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+ROLLBACK TRAN
+DECLARE @ErrorMessage NVARCHAR(4000);
+SELECT @ErrorMessage=ERROR_MESSAGE()
+RAISERROR(@ErrorMessage, 10, 1);
+END CATCH
+Go
+
 
 
 BEGIN TRAN
